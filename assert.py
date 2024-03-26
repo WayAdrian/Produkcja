6a5ab0430b3e134869f66cc2fcea11eea9630c18
unknown <adriansposob@wp.pl  git config --global user.email adriansposob@wp.pl>
2024-03-26 09:40:39 +0100

    drugi

--- assert.py
+++ assert.py
@@ -12,21 +12,10 @@ import datetime
 import pyodbc
 
 
-today = str(datetime.date.today())
-dzis = today.replace("-", "")
-f.pyautogui.PAUSE = 1
-poczatek_testu = time.time()
-
-def next():
-    f.jeden('enter')
-    f.kilka(1, 'down')
-    f.jeden('enter')
-    f.kilka(1, 'down')
 
 
 
 
-D='D-' + dzis
 
 def sprawdz_dokument(typ_ktr, nazwa_ktr):
     zapytanie = "SELECT typ_ktr, Nazwa_ktr FROM kontrah WHERE Nazwa_ktr = ? AND TYP_KTR = ?;"
@@ -35,16 +24,15 @@ def sprawdz_dokument(typ_ktr, nazwa_ktr):
     print(results)
     assert results, f"Dokument '{nazwa_ktr}' o typie '{typ_ktr}' nie istnieje w bazie danych!"
     return results
-    print(results)
 
-f.time.sleep(2)
+
 
 def sprawdz_art(symbol_art, rodz_art):
     zapytanie = "SELECT rodz_art, symbol_art FROM artykuly WHERE symbol_art = ? AND rodz_art = ?;"
     f.cursor.execute(zapytanie, (symbol_art, rodz_art))
     results = f.cursor.fetchall()
     print(results)
-    assert results, f"Dokument '{symbol_art}' o typie '{rodz_art}' nie istnieje w bazie danych!"
+    assert results, f"Artykuł '{symbol_art}' o typie '{rodz_art}' nie istnieje w bazie danych!"
     return results
 
 def sprawdz_tech(symbol_tec, symbol_wyr):
@@ -62,5 +50,3 @@ def sprawdz_poz_dok(symbol_art, ilosc, cena, klucz_dok):
     print(results)
     assert results, f"Pozycja dokumentu '{symbol_art}'  nie istnieje w bazie danych!"
     return results
-
-f.sprawdz_art('S1', "S")
